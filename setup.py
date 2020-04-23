27f1f07202a044b92dd5144bd84a650947e5ea4f
chobe111 <chobe1@naver.com>
2020-04-23 14:58:51 +0900

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='micg',
-    version="1.0.0",
+    version="1.1.0",
     description="Medical Image Changer",
     author="Myung Ki Cho",
     author_email="chobe0719@gmail.com",
